3701e8b4a74f7f82bc8ae9e245ee2193fe2f2912
Eryk Wójcik <drumpixtv@gmail.com>
2025-02-25 22:18:08 +0100

    fixed posthog integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
+"use client";
+import { posthog } from "posthog-js";
+import { Button } from "~/components/ui/button";
+
 export default function HomePage() {
+  const onClick = () => {
+    console.log("clicked");
+    posthog.capture("Test");
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-teal-700 to-emerald-900 text-white">
       <h1 className=" text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
@@ -7,7 +16,9 @@ export default function HomePage() {
       <p className="text-2xl font-bold tracking-tight text-white sm:text-[2rem] pt-5">
         My name is Eryk Wójcik yo.
       </p>
+      <Button onClick={onClick}>
+        Test
+      </Button>
     </main>
   );
 }
-  
